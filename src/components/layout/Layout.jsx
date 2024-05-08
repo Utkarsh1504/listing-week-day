b91fd5f76b070da96a6b9b5d74eaaa7c47f57263
Utkarsh1504 <umishra1504@gmail.com>
2024-05-08 11:31:53 +0530

    basic-layout api-data-fetching

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "../../actions/fetchData";
 import FilterForm from "../filters/FilterForm";
 
 const Layout = () => {
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobData = async () => {
+    try {
+      const response = await fetchData();
+      if (!response) return;
+      setJobs(response.jdList);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobData();
+  }, []);
+
   return (
     <div className="app-container">
+      <h2
+        style={{
+          margin: "20px",
+          marginLeft: "50px",
+          padding: "10px",
+        }}
+      >
+        Weekday Job Listing
+      </h2>
+
       <FilterForm />
+
+      {jobs?.map((job, index) => (
+        <div key={index}>{job.jobRole}</div>
+      ))}
     </div>
   );
 };
