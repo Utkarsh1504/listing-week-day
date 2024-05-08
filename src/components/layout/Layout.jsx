9e6c040a237ef88e1b52bca21189d16dc973e0e4
Utkarsh1504 <umishra1504@gmail.com>
2024-05-08 17:52:43 +0530

    infinite-scrolling

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -11,11 +11,30 @@ const Layout = () => {
     try {
       const response = await fetchData();
       if (!response) return;
+
       setJobs(response.jdList);
     } catch (error) {
       console.log(error);
     }
   };
+
+  // for infinite scroll checking scroll position
+  const intersectionObserver = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 100) {
+      fetchJobData();
+    }
+  };
+
+  // fetch more jobs
+  useEffect(() => {
+    window.addEventListener("scroll", intersectionObserver);
+    // clean up function
+    return () => window.removeEventListener("scroll", intersectionObserver);
+  }, []);
 
   useEffect(() => {
     fetchJobData();
@@ -32,8 +51,9 @@ const Layout = () => {
       >
         Weekday Job Listing
       </h2>
-
-      <FilterForm />
+      <div className="filter-container">
+        <FilterForm />
+      </div>
 
       <div className="jobs-container">
         {jobs?.map((job, index) => (
